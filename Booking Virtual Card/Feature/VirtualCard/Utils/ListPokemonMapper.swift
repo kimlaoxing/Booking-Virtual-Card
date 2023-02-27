28b5d9d7ff4566ea9900f5f9aa89944f50905945
kimlaoxing <kimlaoxing@gmail.com>
2023-02-27 09:09:13 +0700

    remove unused code

--- Booking Virtual Card/Feature/VirtualCard/Utils/ListPokemonMapper.swift
+++ Booking Virtual Card/Feature/VirtualCard/Utils/ListPokemonMapper.swift
@@ -1,13 +1,1 @@
-import Foundation
 
-final class ListAreaMapper {
-    static func listAreaMapper(response: [AreaForSTIDResponse]) -> [ListAreaResult] {
-        return response.map { data in
-            return ListAreaResult(id: data.id ?? "",
-                                  areaName: data.areaName ?? "",
-                                  floorName: data.floorName ?? "",
-                                  buildingName: data.buildingName ?? "",
-                                  location: data.location ?? "")
-        }
-    }
-}
